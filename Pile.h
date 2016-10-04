d8302f7b157ef91dec3d4ef70ed02ba2fab4abdb
sammyson101 <samueldchristensen@gmail.com>
2016-10-03 17:16:23 -0700

    Delete Pile.h

--- Pile.h
+++
@@ -1,9 +0,0 @@
-#include "Pile.h"
-
-using namespace std;
-
-/*move a single card from the source pile's card vector to the function caller's cards vector*/
-void Pile::moveCard(Pile &source){
-    cards.push_back(source.cards.back());
-    source.cards.pop_back();
-}
